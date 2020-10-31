af4d7062b4e0a746e4737f854eb965cc0d99d65f
mun-ahmd <muneeb0ahmed3@gmail.com>
2020-10-31 20:57:03 +0530

    fixed PyQt5 import problem

--- Ui.py
+++ Ui.py
@@ -1,21 +1,21 @@
 from PyQt5 import uic
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTextEdit, QLabel, QTabWidget
+from PyQt5 import QtWidgets as QW
 
 
-class UI(QMainWindow):
+class UI(QW.QMainWindow):
     def __init__(self):
         super(UI,self).__init__()
         uic.loadUi("main_window.ui",self)
 
 
-        self.textedit = self.findChild(QTextEdit,"textEdit")
-        self.label = self.findChild(QLabel,"label")
-        self.tabwidget = self.findChild(QTabWidget,"tabWidget")
+        self.textedit = self.findChild(QW.QTextEdit,"textEdit")
+        self.label = self.findChild(QW.QLabel,"label")
+        self.tabwidget = self.findChild(QW.QTabWidget,"tabWidget")
 
 
 
 
-app = QApplication(sys.argv)
+app = QW.QApplication(sys.argv)
 window = UI()
 app.exec_()
