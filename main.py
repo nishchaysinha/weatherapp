962f115cccd7be1c3cf7fbc280406607e1e712e4
VULKAN64 <61420072+VULKAN64@users.noreply.github.com>
2020-10-31 11:38:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pymysql
+import time
 
 connection=pymysql.connect(host='127.0.0.1',
                              user='root',
                              password='',
                              db='weather')
 
+with connection.cursor() as cursor:
+    base-cmd='INSERT INTO TABLE weather VALUES '
+    cursor.execute(base-cmd + "()")
