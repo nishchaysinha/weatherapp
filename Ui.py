0fff69c1100bed9fbb2271fc44e83ee219664893
Od1n1457 <50940835+Od1n1457@users.noreply.github.com>
2020-11-01 13:26:02 +0530

    Changed UI to call directly

--- Ui.py
+++ Ui.py
@@ -3,19 +3,9 @@ import sys
 from PyQt5 import QtWidgets as QW
 
 
-class UI(QW.QMainWindow):
-    def __init__(self):
-        super(UI,self).__init__()
-        uic.loadUi("main_window.ui",self)
-
-
-        self.textedit = self.findChild(QW.QTextEdit,"textEdit")
-        self.label = self.findChild(QW.QLabel,"label")
-        self.tabwidget = self.findChild(QW.QTabWidget,"tabWidget")
-
-
-
 
 app = QW.QApplication(sys.argv)
-window = UI()
+window = uic.loadUi("main_window.ui")
+
+window.show()
 app.exec_()
